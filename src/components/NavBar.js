1c8944da79fd7b7f65dbf795f2a5c34299cd6126
maurice diong <mdiong00@gmail.com>
2023-12-11 22:39:47 -0500

    getting closer to that web application

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksComp = links.map((link) =>{
+    const hreff = `#${link}`;
+    return(
+      <a key={link} href={hreff}>{link}</a>
+    )
+  }
+  )
+  return <nav>{linksComp}</nav>;
 }
 
 export default NavBar;
